74914b42e62186cbb866d38a0aa93abdf759cc82
Ryan Jones <ryandjones14@gmail.com>
2016-04-04 12:45:55 -0500

    added show page for liked alpacas

--- routes/alpacas.js
+++ routes/alpacas.js
@@ -15,22 +15,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/liked', function(req, res, next){
-  var alpacas = Alpaca.find({likes: {$gt: 0 } }, function(err, alpaca){
-      res.render('alpacas/liked', {title: "Alpacas you have liked!", })
-  })
-})
+   Alpaca.find({likes: {$gt: 0 } }, function(err, alpacas){
+      if (err){
+        console.log(err);
+      } else {
+        console.log(alpacas);
+      }
+      res.render('alpacas/liked', {title: "Alpacas you have liked!", liked: alpacas})
+    })
+ })
 
 router.post('/', function (req, res) {
   // Update a document by another field
   var id = req.body.id;
   var like = req.body.like;
+  var backURL=req.header('Referer') || '/';
   console.log(like);
   Alpaca.findByIdAndUpdate(id, {likes: like}, function(err, alpaca) {
     if (err){
       console.log(err);
     } else {
       // console.log(alpaca);
-      res.redirect('/alpacas');
+      res.redirect(backURL);
     }
   });
 });
